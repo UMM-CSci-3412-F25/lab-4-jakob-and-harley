0a3a8cd2ac956c68da31c71a4d4d26e05179deb0
El Anderson <and10393@umn.edu>
2025-10-04 19:15:37 -0500

    implemented summarize _tree.sh

---
+++ summarize_tree/summarize_tree.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dir="$1"
+
+num_dirs="$(find "$dir" -type d | wc -l | xargs)"
+num_regular="$(find "$dir" -type f | wc -l | xargs)"
+
+echo "There were $num_dirs directories."
+echo "There were $num_regular regular files."
